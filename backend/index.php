765e27850d8f693ed5ed96089ac1b2ce6250279a
hakhverdyannatalya <evahakhverdyan@mail.ru>
2021-06-03 19:57:59 +0400

    инстаграм

--- backend/index.php
+++ backend/index.php
@@ -232,6 +232,85 @@ $APPLICATION->SetTitle("Интернет-магазин \"Одежда\"");
 	)
 );?>
 	</div>
+    <section class="info-block">
+        <div class="info-block__title">
+            Мы в Инстаграм:
+        </div>
+        <?
+        $GLOBALS['arrFilter']  = array('PROPERTY_MAIN_NEWS_VALUE' => "Y");
+        $APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "SORT",
+			1 => "",
+		),
+		"FILTER_NAME" => "arrFilter",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MEDIA_PROPERTY" => "",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "12",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "MAIN_NEWS",
+			1 => "",
+		),
+		"SEARCH_PAGE" => "/search/",
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SLIDER_PROPERTY" => "",
+		"SORT_BY1" => "SORT",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "ASC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"TEMPLATE_THEME" => "blue",
+		"USE_RATING" => "N",
+		"USE_SHARE" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
+
+    </section>
 
 <?
 $now = new DateTime();
