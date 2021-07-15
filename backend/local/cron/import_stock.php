5dbdc9af22c5972127eb7f9abd236bf6e75115be
hakhverdyannatalya <evahakhverdyan@mail.ru>
2021-07-15 14:53:55 +0400

    обновление цен

--- backend/local/cron/import_stock.php
+++ backend/local/cron/import_stock.php
@@ -18,6 +18,7 @@ define('AUTH', '7e47a6fdfa664dbc876fa0310f962e5a');
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule("iblock");
 CModule::IncludeModule('catalog');
+CModule::IncludeModule("sale");
 @set_time_limit(0);
 @ignore_user_abort(true);
 
@@ -26,6 +27,7 @@ define("BX_CRONTAB_SUPPORT", true);
 define("BX_CRONTAB", true);
 CEvent::CheckEvents();
 logger("Начали обновление цен");
+
 $resProducts =  \Bitrix\Iblock\ElementTable::getList([
     "select" => [
         "CODE",
@@ -34,7 +36,8 @@ $resProducts =  \Bitrix\Iblock\ElementTable::getList([
         "PRICE" => "PRICE_LIST.PRICE",
         "QUANTITY" => "PRODUCT.QUANTITY",
         "AVAILABLE" => "PRODUCT.AVAILABLE",
-        "IBLOCK_ID"
+        "IBLOCK_ID",
+         "PRICEID" => "PRICE_LIST.ID"
     ],
     'runtime' => [
         'PRICE_LIST' => [
@@ -53,19 +56,23 @@ $resProducts =  \Bitrix\Iblock\ElementTable::getList([
         ]
     ],
     'filter' => [
-        ">=IBLOCK_ID" => 4
+        "IBLOCK_ID" => [$_ENV['BOOK_BLOCK_ID'], $_ENV['NON_BOOK_BLOCK_ID'], $_ENV['FOOD_BLOCK_ID']],
+        'PRICE_LIST.CATALOG_GROUP_ID' => 1
     ],
 ])->fetchAll();
+
 
 $offers = array_combine(
     array_map(function($item) { return ($item["CODE"]); }, $resProducts),
     array_map(function($item) { return ((object)[
+        'id' => $item['ID'],
         'idtow' => $item['CODE'],
         'available' => $item['AVAILABLE'] === 'Y' ? 1 : 0,
         'price' => $item['PRICE'],
         'stock' => $item['QUANTITY']
     ]); }, $resProducts)
 );
+
 
 for( $i = 0; $i <= count($offers)/100; $i++ ){
     $body['idtows'] = array_slice(array_keys($offers), $i*100, 100);
@@ -90,21 +97,31 @@ for( $i = 0; $i <= count($offers)/100; $i++ ){
     $stocks = json_decode($result)->stocks;
 
     foreach($stocks as $stock){
-        if($stock <> $offers[$stock->idtow]) {
-
-            $key = array_search($stock->idtow, array_column($resProducts, 'CODE'));
-            $elementID = $resProducts[$key]['ID'];
-
+        if($offers[$stock->idtow]){
+            $elementID = $offers[$stock->idtow]->id;
             if($stock->price <> $offers[$stock->idtow]->price){
                 $priceFields = Array(
                     "PRODUCT_ID" => $elementID,
-                    "PRICE" => $stock->price
+                    "CATALOG_GROUP_ID" => 1,
+                    "PRICE" => $stock->price,
+                    "CURRENCY" => "RUB"
                 );
-
-                $result = \Bitrix\Catalog\Model\Price::update($elementID, $priceFields);
+                $dbPrice = \Bitrix\Catalog\Model\Price::getList([
+                    "filter" => array(
+                        "PRODUCT_ID" => $elementID,
+                        "CATALOG_GROUP_ID" => 1
+                    )
+                ]);
+                $obPrice = new \Bitrix\Catalog\Model\Price;
+                if ($arPrice = $dbPrice->Fetch()) {
+                    $result = $obPrice->Update($arPrice["ID"], $priceFields);
+                } else {
+                    $obPrice->Add($priceFields);
+                }
             }
+
             if($stock->available <> $offers[$stock->idtow]->available){
-               \Bitrix\Catalog\Model\Product::update($elementID, ['AVAILABLE' => $stock->available ? "Y" : "N" ]);
+                \Bitrix\Catalog\Model\Product::update($elementID, ['AVAILABLE' => $stock->available ? "Y" : "N" ]);
             }
 
             if($stock->stock <> $offers[$stock->idtow]->stock){
