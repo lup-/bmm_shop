ab44b0c88e1109400561da8b9cf4b6db40cdec41
hakhverdyannatalya <evahakhverdyan@mail.ru>
2021-07-26 21:46:19 +0400

    черновик в яндекс доставку

--- backend/local/php_interface/init.php
+++ backend/local/php_interface/init.php
@@ -1,9 +1,21 @@
 <?php
 AddEventHandler("main", "OnBeforeUserRegister", Array("RegistrationClass", "OnBeforeUserRegisterHandler"));
+AddEventHandler("sale", "OnOrderSave", 'OnOrderSaveHandler');
+
 class RegistrationClass
 {
     function OnBeforeUserRegisterHandler(&$arFields)
     {
         $arFields["LOGIN"] = $arFields["EMAIL"];
+    }
+}
+
+function OnOrderSaveHandler(&$ID, &$arFields, &$orderFields, &$isNew) {
+    include_once $_SERVER["DOCUMENT_ROOT"]."/local/helper/yandexDelivery.php";
+
+    $yandexDelivery = new YandexDelivery();
+
+    if($yandexDelivery->isAllowDeliveryRequest($ID)) {
+        $yandexDelivery->createOrderDraft($ID);
     }
 }
